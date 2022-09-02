b13294ffde09d65483d84a5ef0152810eb2346b0
vanaja <vanajakanagala09@gmail.com>
2022-09-02 17:20:43 +0530

    Addition class

---
+++ src/main/java/Git/Addition.java
@@ -0,0 +1,10 @@
+package Git;
+
+public class Addition {
+    public static void main(String[] args) {
+        int a=30;
+        int b=40;
+        int c=a+b;
+        System.out.println(c);
+    }
+}
