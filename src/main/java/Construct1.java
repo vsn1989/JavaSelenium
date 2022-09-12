25d4029eddc099c283ee7bd7c9a37932bf3f952f
vsn@1989 <vemurinaveen069@gmail.com>
2022-09-12 11:48:54 +0530

    baru code

---
+++ src/main/java/Construct1.java
@@ -0,0 +1,19 @@
+public class Construct1 {
+    int a,b;
+    public Construct1(int a,int b) {
+        this.a = a;
+        this.b = b;
+    }
+        public void add()
+        {
+            int c = a + b;
+            System.out.println(c);
+        }
+        public void sub()
+        {
+            int c=a-b;
+            System.out.println(c);
+
+        }
+
+    }
